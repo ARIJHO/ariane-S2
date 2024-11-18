bfa8224b43c4cedd650bfcb1b3817f05048767fc
ARIJHO <128059950+ARIJHO@users.noreply.github.com>
2024-11-18 11:47:18 -0300

    Add files via upload

--- ariane/main.js
+++ ariane/main.js
@@ -1,5 +1,14 @@
-const numeroSenha= document.querySelector('.parametro-senha-texto');
-let tamanhoSenha= 17;
-numeroSenha.textContent= tamanhoSenha;
-const botoes= document.querySelector('.parametro-senha-botoes');
-botoes[0].onclick=diminuirTamanho;
+const numeroSenha=document.querySelector('.parametro-senha-texto');
+let tamanhoSenha=12;
+numeroSenha.textContent=tamanhoSenha;
+const botoes=document.querySelectorAll('.parametro-senha-botao');
+botoes[0].onclick=aumentarTamanho;
+botoes[1].onclick=diminuirTamanho;
+function diminuirTamanho(){
+    if(tamanhoSenha>=5){    tamanhoSenha=tamanhoSenha-1;
+        numeroSenha.textContent=tamanhoSenha;}
+}
+function aumentarTamanho(){
+    if(tamanhoSenha<=20){    tamanhoSenha=tamanhoSenha+1;
+        numeroSenha.textContent=tamanhoSenha;}
+}
