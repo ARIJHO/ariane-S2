c975bd66c90420e9d7a4c57cd31348f8ee151a16
ARIJHO <128059950+ARIJHO@users.noreply.github.com>
2024-11-25 11:54:41 -0300

    Add files via upload

--- ariane/main.js
+++ ariane/main.js
@@ -2,7 +2,10 @@ const numeroSenha=document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
 const botoes=document.querySelectorAll('.parametro-senha-botao');
+const campoSenha= document.querySelector('#campo-senha');
+const letrasMaiusculas="ABCDEFGHIJKLMNOPQRSTUVWXYZÇ";
 botoes[0].onclick=aumentarTamanho;
+geraSenha();
 botoes[1].onclick=diminuirTamanho;
 function diminuirTamanho(){
     if(tamanhoSenha>=5){    tamanhoSenha=tamanhoSenha-1;
@@ -11,4 +14,14 @@ function diminuirTamanho(){
 function aumentarTamanho(){
     if(tamanhoSenha<=20){    tamanhoSenha=tamanhoSenha+1;
         numeroSenha.textContent=tamanhoSenha;}
+}
+function geraSenha(){
+    let senha=" ";
+    for(let i=O; i<tamanhoSenha; i++){
+    let numeroAleatorio=Math.random()* letrasMaiusculas.length;
+    numeroAleatorio= Math.floor(numeroAleatorio);
+    senha=senha+letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value=senha;
+
 }
